cd04a1133ea65e68e352f959ec9f9cf8bef1de18
velivalab <72046990+velivalab@users.noreply.github.com>
2022-05-20 03:19:37 +0530

    Update secondTest.java

--- src/test/java/com/test/secondTest.java
+++ src/test/java/com/test/secondTest.java
@@ -30,6 +30,18 @@ public class secondTest {
 		throw new SkipException("skipped");
 	}
 	
+	@Test
+	public void multiplyTest() {
+		//This test will check the multiply functionality of the test
+		assertTrue("Sum function not working fine.", calculator.multiply(10, 20) == 200);
+	}
+	
+	@Test
+	public void devideTest() throws Exception {
+		//This test will check the multiply functionality of the test
+		assertTrue("Sum function not working fine.", calculator.divide(20, 10) == 2);
+	}
+	
 	@Test 
 	public void testAdd()
     {
