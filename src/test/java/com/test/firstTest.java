236fb8ef8cb1f2aab27c068f50dcb1784be9d06c
velivalab <72046990+velivalab@users.noreply.github.com>
2022-05-20 03:18:59 +0530

    Update firstTest.java

--- src/test/java/com/test/firstTest.java
+++ src/test/java/com/test/firstTest.java
@@ -62,6 +62,18 @@ public class firstTest {
 	public void emptytest() {
 
 	}
+	
+	@Test
+	public void sumTest() {
+		//This test will check the sum functionality of the test
+		assertTrue("Sum function not working fine.", calculator.sum(10, 20) == 30);
+	}
+	
+	@Test
+	public void subtractTest() {
+		//This test will check the subtract functionality of the test
+		assertTrue("Sum function not working fine.", calculator.subtract(100, 20) == 80);
+	}
 
 	@Test
 	public void failedtest() {
