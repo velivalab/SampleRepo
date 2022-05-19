ad1bc25e4a9c5e69744f4f2a477e63c1518a7152
velivalab <72046990+velivalab@users.noreply.github.com>
2022-05-20 03:29:44 +0530

    Update firstTest.java

--- src/test/java/com/test/firstTest.java
+++ src/test/java/com/test/firstTest.java
@@ -17,6 +17,8 @@ import org.junit.Ignore;
 import org.junit.experimental.categories.Category;
 import org.testng.SkipException;
 
+import com.lib.Library;
+
 public class firstTest {
 
 	
@@ -65,14 +67,8 @@ public class firstTest {
 	
 	@Test
 	public void sumTest() {
-		//This test will check the sum functionality of the test
-		assertTrue("Sum function not working fine.", calculator.sum(10, 20) == 30);
-	}
-	
-	@Test
-	public void subtractTest() {
-		//This test will check the subtract functionality of the test
-		assertTrue("Sum function not working fine.", calculator.subtract(100, 20) == 80);
+		Library classUnderTest = new Library();
+		assertTrue("Sum function not working fine.", classUnderTest.add(10, 20) == 30);
 	}
 
 	@Test
